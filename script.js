e44cc3f44de556fd59fb9238f2e8018b5b28b529
TurtleNav <tourangeau_luc@yahoo.com>
2023-12-01 21:30:36 -0500

    Added Date functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+console.log("hey");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +22,26 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  function renderDateString() {
+    var dayNumber = Number(dayjs().format("D"));
+    var daySuffix;
+    switch (dayNumber) {
+      case 1:
+        daySuffix = "st";
+        break;
+      case 2:
+        daySuffix = "nd";
+        break;
+      case 3:
+        daySuffix = "rd";
+        break;
+      default:
+        daySuffix = "th";
+    }
+    $('p#currentDay').text(dayjs().format(`dddd, MMMM D[${daySuffix}]`));
+  }
+  renderDateString();
 });
+
+console.log("yo");

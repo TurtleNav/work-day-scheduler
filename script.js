4ece08d5624cfca9e8245c8c13889dd9a6bf4ba9
TurtleNav <tourangeau_luc@yahoo.com>
2023-12-01 23:13:28 -0500

    Finished hour block renderer

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-console.log("hey");
+
+// Today's date - updated whenever time related elements are rendered
+var todayDate;
+
+function updateTime() {
+  todayDate = dayjs();
+}
+
+function renderTimeBlock(hour) {
+  // Create parent div
+  /*
+  var pDiv = $("<div>")
+  pDiv.addClass('row time-block');
+  pDiv.attr("id", `hour-${hour}`)
+  */
+
+  var pDiv = $(`<div id="hour-${hour}" class="row time-block">`);
+
+  if (hour < todayDate.format('H')) {
+    pDiv.addClass("past");
+  } else if (hour === todayDate.format('H')) {
+    pDiv.addClass("present");
+  } else {
+    pDiv.addClass("future");
+  }
+
+  if (hour > 12) {
+    end = "PM";
+    hour -= 12;
+  } else if (hour === 12) {
+    end = "PM";
+  } else {
+    end = "AM";
+  }
+
+  var textDiv = $('<div class="col-2 col-md-1 hour text-center py-3">');
+  //var $textDiv = $('<class="col-2 col-md-1 hour text-center py-3">');
+  //$textDiv.text(`${hour}${end}`);
+  textDiv.text(`${hour}${end}`);
+
+  //$pDiv.append($textDiv);
+  pDiv.append(textDiv);
+
+  //$pDiv.append($('<textarea class="col-8 col-md-10 description" rows="3">'));
+  pDiv.append($('<textarea class="col-8 col-md-10 description" rows="3">'));
+
+  var buttonEl = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">');
+  buttonEl.append($('<i class="fas fa-save" aria-hidden="true">'));
+
+  //$pDiv.append(buttonEl);
+  pDiv.append(buttonEl);
+  
+  //$('div.px-5').append($pDiv);
+  $('div.px-5').append(pDiv);
+
+}
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -23,8 +78,10 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  function renderDateString() {
-    var dayNumber = Number(dayjs().format("D"));
+
+  function renderDate() {
+    updateTime();
+    var dayNumber = Number(todayDate.format("D"));
     var daySuffix;
     switch (dayNumber) {
       case 1:
@@ -39,9 +96,14 @@ $(function () {
       default:
         daySuffix = "th";
     }
-    $('p#currentDay').text(dayjs().format(`dddd, MMMM D[${daySuffix}]`));
+    $('p#currentDay').text(todayDate.format(`dddd, MMMM D[${daySuffix}]`));
   }
-  renderDateString();
+  renderDate();
+  for (let i=9; i<18; i++) {
+    renderTimeBlock(i);
+  }
+
+
 });
 
 console.log("yo");
